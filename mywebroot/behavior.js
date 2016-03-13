83526e146a8d46c09f372f20d849b99e2d521219
David Skoda <dskoda1@binghamton.edu>
2016-03-13 00:08:29 +0000

    Commented out the entire behavior.js file. Also want to extend the breadcrumb functionality to provide hrefs back to pages that are being saved in the map

--- mywebroot/behavior.js
+++ mywebroot/behavior.js
@@ -2,86 +2,142 @@
 var app = app || {};
 app.view = app.view || {};
 app.router = app.router || {};
+//Not using this ATM?
 app.ajaxURL = "Documents/Head/mywebroot/";
+
+//Use this map when setting breadcrumb text. it allows the change
+//from plain non-descriptive html folder/file names to make
+//them more human readable. Any additions here will be reflected
+//without change to any code
 app.breadcrumbsMap = {
   requirements: 'Submission Requirements',
   expectation: 'Grading Policy',
   471: 'Home',
-  '471HaskellIndex': 'Haskell'
+  '471HaskellIndex': 'Haskell',
+  '471RuntimeIndex': 'Language Design Issues'
 
 }
 
 //Define the main view here
 app.PageView = Backbone.View.extend({
 
+  //The page element gets passed into from options
   el: null,
 
   initialize: function(options) {
-
+    //Save the el, and write loading text
     this.el = options.el;
     this.$el.html("Loading..");
 
   },
+  
+  //This function does the heavy lifting of the ajax call to 
+  //get the actual html page
   loadPage: function(url) {
+    
+    //Create the template variable, and save a ref to 'this'
     var template;
     var that = this;
-    var req = $.get(url, function(data) {
+    $.get(url, function(data) {
+      
         //Load the requested page in
         //Need to mutate the data to have line breaks in it
         //Only if the file is not an html file
         if (url.indexOf('.html') == -1) {
           data = that.insertLineBreaks(data);
         }
+        
+        //Use underscores templating engine to create the view
         template = _.template(data, {});
-        this.$el.html(template)
-      }.bind(this))
+        
+        //Set the html as the underscore view
+        that.$el.html(template)
+      })
+      //When the ajax call fails..
       .fail(function() {
-        //Load the 404 page
+        
+        //Load the 404 page if requested URL not found
         $.get('404.html', function(data) {
+          
+          //Empty breadcrumb and render 404 page
           $('.breadcrumb').empty();
           template = _.template(data, {});
           that.$el.html(template)
+          
+          //Assuming the 404 wont fail to laod, cause if it does
+          //The problem is out of our hands
         })
       })
+      //No matter whether success or fail, set window to be at top
       .always(function(){
         window.scrollTo(0, 0);
       })
   },
+  
+  //Update the bread crumbs at the top of the page
   renderBreadCrumbs: function(urlArr) {
+    
+    //Get a jquery reference to the DOM element, and empty it
     var bc = $('.breadcrumb')
     bc.empty();
-    var crumbs = [];
-    _.each(urlArr, function(page, i) {
-
-      if (i == urlArr.length - 1 && page.indexOf('.html') > -1) {
-        page = page.substr(0, page.length - 5);
+    
+    //for each element in the url, render a breadcrumb appended to the 
+    //dom element with classname 'breadcrumb'
+    _.each(urlArr, function(subPath, i) {
+  
+      //If looking at last index of the array, and file is html then
+      //cut off the html for prettier looking naming
+      if (i == urlArr.length - 1 && subPath.indexOf('.html') > -1) {
+        subPath = subPath.substr(0, subPath.length - 5);
       }
-      if (app.breadcrumbsMap[page]) {
-        page = app.breadcrumbsMap[page];
+      
+      //Also, check here whether or not we have a more human readable
+      //name for this specific subPath in the breadcrumbs Map at the top
+      //of this script. If so, replace
+      if (app.breadcrumbsMap[subPath]) {
+        subPath = app.breadcrumbsMap[subPath];
       }
-      page = '<li>' + page + '</li>';
-      bc.append(page);
-
+      
+      //Finally, put the actual list tags needed for proper css to be applied
+      subPath = '<li>' + subPath + '</li>';
+      
+      //And last but not least, append the new html string to the subPath
+      bc.append(subPath);
     })
   },
   insertLineBreaks: function(data) {
+    
     //Need to mutate the data to have line breaks in it
-    //Only if the file is not an html file
+    //Only called if the file is not an html file, aka any code
+    //file runs through this function
+    
+    //Instantiate the string that will be returned to be used as
+    //HTML
     var page = '';
+    
     _.each(data, function(char) {
+      
+      //Find the line breaks and transform them into html line breaks
       if (char == '\n') {
         char = '<br>';
       }
+      
+      //Append to the return string
       page += char;
     })
+    
     return page;
   }
 
 })
 
-
+//This object controls what backbone does with url requests
 app.MainRouter = Backbone.Router.extend({
 
+  //Here are all the routes we have defined. Unfortuneately 
+  //We cant make this any more dynamic(aka condense all five
+  //into just one and split based on '/') because of the way 
+  //backbone passes route arguments.
   routes: {
     ':one': 'one',
     ':one/:two': 'two',
@@ -89,11 +145,13 @@ app.MainRouter = Backbone.Router.extend({
     ':one/:two/:three/:four': 'four',
     ':one/:two/:three/:four/:five': 'five',
 
+    //Renders the home page
     '': 'default',
-
-
-
   },
+  
+  //For each of the functions below, we first call the load page 
+  //function which makes the ajax call, and secondly we pass the 
+  //url subPaths as an array to the render breadcrumbs function.
   one: function(first) {
     app.View.loadPage(first);
     app.View.renderBreadCrumbs([first]);
@@ -114,21 +172,28 @@ app.MainRouter = Backbone.Router.extend({
     app.View.loadPage(first + '/' + second + '/' + third + '/' + fourth + '/' + fifth);
     app.View.renderBreadCrumbs([first, second, third, fourth, fifth]);
   },
-  default: function(pathName) {
+  
+  //Don't care about path name, just render the home page
+  default: function() {
     app.View.loadPage('home.html');
     app.View.renderBreadCrumbs();
-  },
-
-
+  }
 })
 
+//This function runs when the DOM has completed rendering. It 
+//ensures backbone doesn't try doing stuff to the el without it 
+//actually being on the page yet
 $(function() {
 
-
+  //Instantiate our page view into the app namespace
   app.View = new app.PageView({
     el: "#pageContent"
   })
+  
+  //Instantiate the router also into the app namespace
   app.Router = new app.MainRouter();
+  
+  //Begin tracking of history, so that back/forward actions
+  //in the browser work as expected
   Backbone.history.start({});
-
 })
